1bd148a42bbbcb927ea116da30f143b8dbe3a4c3
LucasBonna <lucasbonnafavaro@gmail.com>
2025-05-21 00:15:03 -0300

    feat: add sidebar
    - Updated package.json to include new Radix UI components and updated existing dependencies.
    - Modified globals.css to change background and sidebar colors for dark mode.
    - Refactored Input component to use a functional component instead of forwardRef for better readability.

---
+++ frontend/app/chat/layout.tsx
@@ -0,0 +1,31 @@
+import { SidebarProvider, SidebarTrigger, SidebarInset } from '@/components/ui/sidebar'
+import './globals.css'
+import { AppSidebar } from '@/components/app-sidebar'
+import { ThemeProvider } from '@/components/theme-provider'
+
+export default function RootLayout({
+	children,
+}: {
+	children: React.ReactNode
+}) {
+	return (
+		<html lang="en" suppressHydrationWarning>
+			<body>
+				<ThemeProvider 
+					attribute="class"
+					defaultTheme="system"
+					enableSystem
+					disableTransitionOnChange
+					>
+					<SidebarProvider>
+						<AppSidebar />
+						<SidebarInset>
+							<SidebarTrigger />
+							{children}
+						</SidebarInset>
+					</SidebarProvider>
+				</ThemeProvider>
+			</body>
+		</html>
+	)
+}
